6e2ad40aabbdf75af2275b1eea106ed8a31d0693
durgesh awashti <133563743+awasthi16@users.noreply.github.com>
2024-04-20 11:44:59 +0530

    Add files via upload

---
+++ git search-api/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const product = require("./product");
+require("./config");
+const app = express();
+app.use(express.json());
+app.get("/search/:key", async (req, resp) => {
+  console.log(req.params.key);
+  let data = await product.find({
+    "$or": [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ]
+  }); 
+  resp.send(data)
+});
+app.listen(5000);
